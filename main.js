12eb18e764edc606271fc0b6095d1ec6d2059302
PVS KIRAN <PVS KIRAN>
2016-08-25 08:29:34 +0530

    basics over

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
+/*
 var operations = require("./vehicleModel");
 var http = require("http");
 
 console.log(operations.add());
+*/
+
+function getData() {
+    var emp = db.query("select* from emp");
+
+    console.log(emp);
+}
+
+function getNonBlockingData() {
+    db.query("select * from emp", function (err, data) {
+        console.log(data);
+    });
+    console.log("I am done")
+
+}
