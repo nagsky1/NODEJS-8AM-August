7d03b2431abd9408ccffa33bea2469d5401cd066
PVS KIRAN <PVS KIRAN>
2016-09-09 08:14:50 +0530

    added code

--- InventoryApp/server.js
+++ InventoryApp/server.js
@@ -1,18 +1,39 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine', 'ejs');
+app.set("views", "./app/views");
+app.use(express.static('./public'));
+
 // respond with "hello world" when a GET request is made to the homepage
-app.get('/', function(req, res) {
-  res.send('hello world');
+app.get('/', function (req, res) {
+    var users = [{
+            name: "kiran",
+            age: 30,
+            SSN: "1234567890"
+        },
+        {
+            name: "kiran",
+            age: 30,
+            SSN: "1234567890"
+        },
+        {
+            name: "kiran",
+            age: 30,
+            SSN: "1234567890"
+        }]
+    res.render('index', {
+        data: users
+    });
 });
-app.get('/index', function(req, res) {
-  res.send('<h1>I am the index page');
+app.get('/index', function (req, res) {
+    res.send('<h1>I am the index page');
 });
-app.get('/about', function(req, res) {
-  res.send('<h1>I am the about page');
+app.get('/about', function (req, res) {
+    res.render("about");
 });
-app.get('/ab?cd', function(req, res) {
-  res.send('ab?cd');
+app.get('/ab?cd', function (req, res) {
+    res.send('ab?cd');
 });
 app.listen(9000);
 console.log("application running on port 9000");
